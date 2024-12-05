c9f988de1e13b818645ee11eb0663689ae8de721
y1niann <i@en1an.com>
2024-12-05 02:20:41 -0500

    notes

--- part2-notes/src/App.js
+++ part2-notes/src/App.js
@@ -1,6 +1,6 @@
 import { useState, useEffect} from 'react'
 import Note from './components/Note'
-import axios from 'axios'
+import noteService from './services/notes'
 
 const App = () => {
   const [notes, setNotes] = useState([]) //应用的笔记列表状态
@@ -8,15 +8,13 @@ const App = () => {
   const [showAll, setShowAll] = useState(false)//展示的列表
 
   //获取数据库的notes列表
-  useEffect(() => {
-    console.log('effect')
-    axios
-      .get('http://203.55.176.209:3001/notes')
-      .then(response => {
-        console.log('promise fulfilled')
-        setNotes(response.data)
-      })
-  }, [])
+  useEffect(()=>{
+    noteService
+    .getAll()
+    .then(initialNotes=>{
+      setNotes(initialNotes)
+    })
+  },[])
   console.log('render', notes.length, 'notes')
 
   //增加新笔记的事件处理
@@ -28,14 +26,14 @@ const App = () => {
       important: Math.random() > 0.5,}
     
       //将新笔记发送到数据库
-      axios
-      .post('http://203.55.176.209:3001/notes',noteObject)
-      .then(response=>{
-        setNotes(notes.concat(response.data))//将res的数据加入到列表
+      noteService.
+      create(noteObject)
+      .then(returnedNote=>{
+        setNotes(notes.concat(returnedNote))//将res的数据加入到列表
         setNewNote('')//重置输入框
   })
-
   }
+  
   //处理表单变化，注册事件处理
   const handleNoteChange = (event) => {
     setNewNote(event.target.value)
@@ -49,13 +47,14 @@ const App = () => {
   //查找目标的ID
   const toggleImportanceOf=(id)=>{
     const note = notes.find(n=> n.id === id)
-    const changedNote= {...note, important :! note.important}//处理重要性切换
+    const changedNote= { ...note, important :! note.important}//处理重要性切换
 
     //变更数据库
-    axios
-    .put(`http://203.55.176.209:3001/notes/${id}`,changedNote)
-    .then(response=>{setNotes(notes.map(note => note.id !== id ? note : response.data))})
-
+    noteService
+    .update(id,changedNote)
+    .then(returnedNote=>{
+      setNotes(notes.map(note=> note.id !== id? note : returnedNote))
+    })
   }
 
   return (
